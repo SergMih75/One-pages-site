d7e1a465dafb57ad5d4ac568f965caaa7effde99
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2022-10-03 23:48:31 +0300

    Download pages One window site

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.querySelector('.discount-copy-code').addEventListener('click', function (){
+    discount = 'OBOGREV10%';
+    alert('Промокод скопирован. Переходите на сайт для использования.');
+    document.querySelector('.discount-copy-code').style.display = 'none';
+});
